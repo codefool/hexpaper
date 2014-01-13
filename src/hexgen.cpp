ce7049a2a4480846ba2eca4dd2e0ea6dcb22907d
codefool <ghester@codefool.org>
2014-01-12 20:50:10 -0600

    Checkpoint

---
+++ src/hexgen.cpp
@@ -0,0 +1,25 @@
+/*
+ * hexgen.cpp
+ *
+ *  Created on: Jan 12, 2014
+ *      Author: ghester
+ */
+
+#include <iostream>
+#include <vector>
+#include "hexpaper"
+
+namespace org {
+namespace codefool {
+namespace hexpaper {
+
+std::ostream& operator << ( std::ostream& os, const HexInfo& hi )
+{
+
+
+	return os;
+}
+
+} // end ns hexpaper
+} // end ns codefool
+} // end ns org
